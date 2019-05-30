afbe4be5ed07f2fb75f8cad482632254d9114fd7
Newteq <newt.fourie@entelect.co.za>
2019-05-30 11:25:58 +0200

    testing ui-router

---
+++ ui-routing.js
@@ -0,0 +1,48 @@
+var app = angular.module("ui-routing", []);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when("/", {
+			templateUrl: "templates/main.html"
+		})
+		.when("/hello", {
+			templateUrl: "templates/hello.html"
+		})
+		.when("/more", {
+			templateUrl: "templates/more.html",
+		});
+});
+
+app.controller("hello", function ($scope, generator) {
+	$scope.world = "hello world and also ---"
+	generator.gen().forEach(element => {
+		$scope.world += ` ${element}`;
+	});
+});
+app.controller("more", function ($scope) {
+	$scope.more = "more";
+});
+
+app.service('generator', function() {
+  this.gen = function (x) {
+    return [
+			"Mark",
+			"Nick",
+			"Dane",
+			"Dave",
+		]
+  }
+});
+
+app.directive("testComponent", function() {
+	return {
+		templateUrl: 'templates/controllers/test.html',
+		restrict: "E"
+	}
+});
+
+app.filter("makeBig", function() {
+	return function(x) {
+		return x.toUpperCase();
+	};
+});
